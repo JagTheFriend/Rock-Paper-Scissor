50b66f846dc018fd0b7ee60677f4faa7187b3e4a
JagadishMohanty2 <jagadish.mohanty@iesmail.com>
2020-09-04 20:57:48 +0200

    Made the function,Get the numbers from the user

--- Fibonacci.py
+++ Fibonacci.py
@@ -4,3 +4,9 @@ which takes nth starting number,nth ending number and
 nth number of times repeated.
 """
 
+def Fibonacci(start,end,step):
+    for i in range(step):
+        sums = start + end
+        print(sums,end=" ")
+        end = start
+        start = sums
