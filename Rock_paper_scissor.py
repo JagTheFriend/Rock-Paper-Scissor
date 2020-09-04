58000e47e7d62ec5b30b4c6e681dc67e07e42589
JagadishMohanty2 <jagadish.mohanty@iesmail.com>
2020-09-04 22:06:38 +0200

    Got Device Input,Did comparision,Find bugs now

--- Rock_paper_scissor.py
+++ Rock_paper_scissor.py
@@ -4,15 +4,35 @@ get userinput -> between (1,2,3)
 make random number between(1,2,3) -> computer input
 start making comparision to find out who lost,won,ties
 """
-def get_input():
+import random as r
+
+def user_input():
     print("Welcome to Rock Paper scissor")
     try:
         print("1) Rock\n2) Paper\n3) Scissor")
         number = input(": ")
     except ValueError: 
         print("Please enter a number !!")
-        return get_input()
+        return user_input()
 
     else:
-        pass
+        return device_input(number)
 
+def device_input(user:int):
+    device = r.randint(1, 3)
+
+    if user == device:
+        return "It is a tie"
+    
+    elif user == 1 and device == 2:
+        return "Rock vs Paper\nYou lost :("
+
+    elif user == 2 and device == 3:
+        return "Paper vs Scissor\nYou lost :("
+    
+    elif user == 2 and device == 1:
+        return "Paper vs Rock\nYou Won :D"
+
+    elif user == 3 and device == 2:
+        return "Scissor vs Paper\nYou Won :D"
+    
