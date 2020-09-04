a5f1a812fa4583ddc7f9c4b539fa0b70ecc58473
JagadishMohanty2 <jagadish.mohanty@iesmail.com>
2020-09-04 22:00:02 +0200

    Got user input get device input

--- Rock_paper_scissor.py
+++ Rock_paper_scissor.py
@@ -4,3 +4,15 @@ get userinput -> between (1,2,3)
 make random number between(1,2,3) -> computer input
 start making comparision to find out who lost,won,ties
 """
+def get_input():
+    print("Welcome to Rock Paper scissor")
+    try:
+        print("1) Rock\n2) Paper\n3) Scissor")
+        number = input(": ")
+    except ValueError: 
+        print("Please enter a number !!")
+        return get_input()
+
+    else:
+        pass
+
